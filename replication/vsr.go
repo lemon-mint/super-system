04e8a44af837f51303cefe4386e2cc66f3a3418c
lemon-mint <ice.lemons.mint@gmail.com>
2023-01-17 11:52:06 +0900

    OnPropose

--- replication/vsr.go
+++ replication/vsr.go
@@ -1,5 +1,9 @@
 package replication
 
+import (
+	"errors"
+)
+
 type Config struct {
 	Peers             []uint64
 	ViewChangeTimeout uint64
@@ -15,9 +19,20 @@ const (
 	Status_Recovering
 )
 
+var (
+	ErrStatusNotNormal = errors.New("Not in normal state")
+	ErrNotALeader      = errors.New("Not a leader")
+	ErrReplay          = errors.New("Replay")
+)
+
 type ClientEntry struct {
 	SequenceNumber uint64
 	ResponseCache  CacheEntry
+}
+
+type OperationEntry struct {
+	OperationNumber uint64
+	Operation       []byte
 }
 
 type VSRState struct {
@@ -31,11 +46,66 @@ type VSRState struct {
 	Status Status // Normal, ViewChange, Recovering
 
 	OperationNumber uint64
-	CommitNumber    uint64
+	CommitNumberMAX uint64
+	CommitNumberMIN uint64
 
-	OpLog       MemoryLog[ClientEntry] // The log of operations
+	OpLog       MemoryLog[OperationEntry] // The log of operations
 	ClientTable map[uint64]ClientEntry
 
 	HeartbeatTick  uint64
 	ViewChangeTick uint64
+}
+
+func (v *VSRState) Leader() uint64 {
+	return v.Configuration.Peers[0]
+}
+
+func (v *VSRState) OnPropose(ClientID uint64, seq uint64, op []byte) (opn uint64, err error) {
+	// Assert: v.Status == Status_Normal
+	if v.Status != Status_Normal {
+		err = ErrStatusNotNormal
+		return
+	}
+
+	// Assert: v.ViewNumber == v.StableView
+	if v.ViewNumber != v.StableView {
+		err = ErrStatusNotNormal
+		return
+	}
+
+	// Assert: v.Leader() == v.NodeID
+	if v.Leader() != v.NodeID {
+		err = ErrNotALeader
+		return
+		// TODO: redirect to leader
+	}
+
+	// Check if client exists
+	if _, ok := v.ClientTable[ClientID]; !ok {
+		v.ClientTable[ClientID] = ClientEntry{0, CacheEntry{0, 0}}
+	}
+
+	client := v.ClientTable[ClientID]
+
+	// Assert: seq > client.SequenceNumber
+	if seq <= client.SequenceNumber {
+		err = ErrReplay
+		return
+	}
+
+	// Update client sequence number
+	client.SequenceNumber = seq
+	v.ClientTable[ClientID] = client
+
+	// Increment operation number
+	v.OperationNumber++
+	// Assign operation number to operation
+	opn = v.OperationNumber
+
+	// Append operation to log
+	v.OpLog.Append(OperationEntry{opn, op}, opn)
+
+	// Send operation to all replicas
+	// TODO: send to all replicas
+	return
 }
